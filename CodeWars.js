564a70d2dae57c017536fa01ad89d52fd7c5ae39
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-08-17 09:21:36 -0700

    completed daily coding challenge

--- CodeWars.js
+++ CodeWars.js
@@ -159,8 +159,48 @@ const theLongest = (s1, s2) => {
 
 const longest = (s1, s2) => {
 
-  let unique =[...new Set((s1 + s2).split(''))]
+  let unique = [...new Set((s1 + s2).split(''))]
   return unique.join('')
 }
 
-console.log(longest("aretheyhere", "yestheyarehere"))
+// console.log(longest("aretheyhere", "yestheyarehere"))
+
+
+
+// -------------------------------//
+// finds the next integral perfect square after the one passed as a parameter
+//If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+function findNextSquare(sq) {
+  let num = Math.sqrt(sq)
+
+  if (Math.floor(num) < num) {
+    return -1
+  }
+  return (num + 1) * (num + 1)
+
+}
+
+// console.log(findNextSquare(120))
+
+
+// -------------------------------//
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+function printerError(s) {
+  let regex = /[n-z]/g
+  let error = s.match(regex)
+
+  if (!error) {
+    return 0 + "/" + s.length
+  }
+
+  return error.length + "/" + s.length
+}
+
+// let s = "aaabbbbhaijjjm"
+// console.log(printerError(s))
