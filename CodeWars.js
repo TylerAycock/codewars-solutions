7340188490f51351cff8f75e3e57a0c25690e9d9
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-10-17 09:37:17 -0700

    completed daily challenge

--- CodeWars.js
+++ CodeWars.js
@@ -1049,10 +1049,22 @@ function toCamelCase(str) {
   }
 }
 
-console.log(toCamelCase(''))
-console.log(toCamelCase("the_stealth_warrior")) //"theStealthWarrior"
-console.log(toCamelCase("The-Stealth-Warrior")),  //"TheStealthWarrior"
-console.log(toCamelCase("A-B-C"))  //"ABC"
+// console.log(toCamelCase(''))
+// console.log(toCamelCase("the_stealth_warrior")) //"theStealthWarrior"
+// console.log(toCamelCase("The-Stealth-Warrior")),  //"TheStealthWarrior"
+// console.log(toCamelCase("A-B-C"))  //"ABC"
 
 
+// -------------------------------//
 
+// Implement the function unique_in_order which takes as argument a sequence and returns 
+// a list of items without any elements with the same value next to each other and 
+// preserving the original order of elements.
+
+var uniqueInOrder = function (iterable) {
+  return [...iterable].filter((item, index) => item !== iterable[index + 1])
+}
+
+// console.log(uniqueInOrder('AAAABBBCCDAABBB'))// ['A', 'B', 'C', 'D', 'A', 'B']
+// console.log(uniqueInOrder('ABBCcAD'))        //['A', 'B', 'C', 'c', 'A', 'D']
+// console.log(uniqueInOrder([1, 2, 2, 3, 3]))     //[1,2,3]
